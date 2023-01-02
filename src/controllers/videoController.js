a0dd6aa2c7a57fcd39269229417eb0416ba05eb7
Choi Eun Jeong <qhdrn67@naver.com>
2023-01-02 21:24:55 +0900

    Home Video Query

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,42 +1,30 @@
-const videos = [
-  {
-    title: 'First Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes ago',
-    views: 59,
-    id: 1,
-  },
-  {
-    title: 'Second Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes ago',
-    views: 59,
-    id: 1,
-  },
-];
+import Video from '../models/Video';
 
-export const trending = (req, res) => {
-  return res.render('home', { pageTitle: 'Home', videos });
+export const home = async (req, res) => {
+  // Video.find({}, (err, videos) =>
+  //   res.render('home', { pageTitle: 'Home', videos })
+  // );
+  try {
+    const videos = await Video.find({});
+    return res.render('home', { pageTitle: 'Home', videos });
+  } catch (err) {
+    return res.render('Server Error');
+  }
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render('watch', { pageTitle: `Watching: ${video.title}`, video });
+  return res.render('watch', { pageTitle: `Watching: ` });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render('edit', { pageTitle: `Editing: ${video.title}`, video });
+  return res.render('edit', { pageTitle: `Editing: ` });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -46,14 +34,5 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: 'just now',
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect('/');
 };
