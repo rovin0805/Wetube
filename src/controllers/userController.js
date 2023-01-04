c929e5c082ccf61be5e002dc60e3918c970d27f7
Choi Eun Jeong <qhdrn67@naver.com>
2023-01-04 10:11:42 +0900

    set up : create account form & router & controller & scrhema

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,16 @@
-export const join = (req, res) => res.send('Join');
+import User from '../models/User';
+
+export const getJoin = (req, res) =>
+  res.render('join', { pageTitle: 'Create Account' });
+
+export const postJoin = async (req, res) => {
+  try {
+    await User.create(req.body);
+    res.redirect('/login');
+  } catch (err) {
+    res.send('Create Account Error');
+  }
+};
 
 export const edit = (req, res) => res.send('Edit User');
 
