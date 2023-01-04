496d2d4c707ef62f59679b8ecd25be08340dd229
Choi Eun Jeong <qhdrn67@naver.com>
2023-01-04 11:01:47 +0900

    join form validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,24 @@ export const getJoin = (req, res) =>
 
 export const postJoin = async (req, res) => {
   try {
-    await User.create(req.body);
+    const { email, username, password, password2, location } = req.body;
+
+    if (password !== password2) {
+      return res.render('join', {
+        pageTitle: 'Create Account',
+        error: 'Password confirmation does not match',
+      });
+    }
+
+    const existingUser = await User.exists({ $or: [{ email }, { username }] });
+    if (existingUser) {
+      return res.render('join', {
+        pageTitle: 'Create Account',
+        error: 'The username or email is already taken',
+      });
+    }
+
+    await User.create({ email, username, password, location });
     res.redirect('/login');
   } catch (err) {
     res.send('Create Account Error');
