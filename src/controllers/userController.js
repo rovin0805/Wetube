d3fc85d29770bf9efa297c4f2fd5bc01cab90e4b
Choi Eun Jeong <qhdrn67@naver.com>
2023-01-05 20:53:42 +0900

    logout

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -143,10 +143,11 @@ export const finishGithubLogin = async (req, res) => {
   }
 };
 
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect('/');
+};
+
 export const edit = (req, res) => res.send('Edit User');
-
-export const remove = (req, res) => res.send('Remove User');
-
-export const logout = (req, res) => res.send('Log out');
 
 export const see = (req, res) => res.send('See User');
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import {
-  remove,
   edit,
   logout,
   see,
@@ -12,7 +11,6 @@ const userRouter = express.Router();
 
 userRouter.get('/logout', logout);
 userRouter.get('/edit', edit);
-userRouter.get('/delete', remove);
 userRouter.get(':id', see);
 userRouter.get('/auth/github', startGithubLogin);
 userRouter.get('/auth/github/callback', finishGithubLogin);
