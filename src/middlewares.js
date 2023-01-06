741faba0ba973367e113a032ae89a89985b8cdf7
Choi Eun Jeong <qhdrn67@naver.com>
2023-01-06 15:44:37 +0900

    Protector and Public Middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,3 +4,19 @@ export const localMiddleware = (req, res, next) => {
   res.locals.siteName = 'Wetube';
   next();
 };
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,13 +7,18 @@ import {
   getEdit,
   postEdit,
 } from '../controllers/userController';
+import { protectorMiddleware, publicOnlyMiddleware } from '../middlewares';
 
 const userRouter = express.Router();
 
-userRouter.get('/logout', logout);
-userRouter.route('/edit').get(getEdit).post(postEdit);
+userRouter.get('/logout', protectorMiddleware, logout);
+userRouter.route('/edit').all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get(':id', see);
-userRouter.get('/auth/github', startGithubLogin);
-userRouter.get('/auth/github/callback', finishGithubLogin);
+userRouter.get('/auth/github', publicOnlyMiddleware, startGithubLogin);
+userRouter.get(
+  '/auth/github/callback',
+  publicOnlyMiddleware,
+  finishGithubLogin
+);
 
 export default userRouter;
