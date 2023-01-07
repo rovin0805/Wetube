2a37693352b9658b1ace3839ed67a6734dba07b1
Choi Eun Jeong <qhdrn67@naver.com>
2023-01-07 15:59:32 +0900

    refactor : video owner with populate

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -16,14 +16,13 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   try {
     const { id } = req.params;
-    const video = await Video.findById(id);
-    const owner = await User.findById(video.owner);
+    const video = await (await Video.findById(id)).populate('owner');
     if (!video) {
       return res
         .status(404)
         .render('404', { pageTitle: 'Video Not Found 404' });
     }
-    return res.render('watch', { pageTitle: video.title, video, owner });
+    return res.render('watch', { pageTitle: video.title, video });
   } catch (err) {
     return res.send('Watch Video Error');
   }
