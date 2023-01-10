631d9662a4ee2a4e765ba575d99edbea95786650
Choi Eun Jeong <qhdrn67@naver.com>
2023-01-10 11:02:05 +0900

    fix : player events & add styling

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,11 +1,14 @@
 const video = document.querySelector('video');
 const playBtn = document.getElementById('play');
+const playBtnIcon = playBtn.querySelector('i');
 const muteBtn = document.getElementById('mute');
+const muteBtnIcon = muteBtn.querySelector('i');
 const currentTime = document.getElementById('currentTime');
 const totalTime = document.getElementById('totalTime');
 const volumeRange = document.getElementById('volume');
 const timeline = document.getElementById('timeline');
 const fullScreenBtn = document.getElementById('fullScreen');
+const fullScreenIcon = fullScreenBtn.querySelector('i');
 const videoContainer = document.getElementById('videoContainer');
 const videoControls = document.getElementById('videoControls');
 
@@ -21,12 +24,14 @@ const handlePlay = () => {
   } else {
     video.pause();
   }
-  playBtn.innerText = video.paused ? 'Play' : 'Pause';
+  playBtnIcon.classList = video.paused ? 'fas fa-play' : 'fas fa-pause';
 };
 
 const handleMute = () => {
-  muteBtn.innerText = video.muted ? 'Mute' : 'Unmute';
   video.muted = !video.muted;
+  muteBtnIcon.classList = video.muted
+    ? 'fas fa-volume-mute'
+    : 'fas fa-volume-up';
   volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
@@ -37,11 +42,11 @@ const handleInputVolume = (event) => {
   volumeValue = value;
   video.volume = value;
   if (Number(value) === 0) {
-    muteBtn.innerText = 'Unmute';
+    muteBtnIcon.classList = 'fas fa-volume-mute';
     video.muted = true;
   } else {
+    muteBtnIcon.classList = 'fas fa-volume-up';
     video.muted = false;
-    muteBtn.innerText = 'Mute';
   }
 };
 
@@ -83,13 +88,13 @@ const handleTimelineMouseup = () => {
 };
 
 const handleFullScreen = () => {
-  const isFullScreen = document.fullscreenElement();
+  const isFullScreen = document.fullscreenElement;
   if (isFullScreen) {
-    videoContainer.requestFullscreen();
-    fullScreenBtn.innerText = 'Enter Full Screen';
-  } else {
     document.exitFullscreen();
-    fullScreenBtn.innerText = 'Exit Full Screen';
+    fullScreenIcon.classList = 'fas fa-expand';
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenIcon.classList = 'fas fa-compress';
   }
 };
 
@@ -102,6 +107,7 @@ const handleMouseMove = () => {
     clearTimeout(controlsMovementTimeoutId);
     controlsMovementTimeoutId = null;
   }
+  videoControls.classList.add('showing');
   controlsMovementTimeoutId = setTimeout(hideControls, 3000);
 };
 
@@ -116,14 +122,19 @@ window.addEventListener(
   'keydown',
   (event) => event.code === 'Enter' && handlePlay()
 );
+document.addEventListener(
+  'keyup',
+  (event) => event.code === 'Space' && handlePlay()
+);
 muteBtn.addEventListener('click', handleMute);
 volumeRange.addEventListener('input', handleInputVolume);
 video.readyState
   ? handleLoadedMetaData()
   : video.addEventListener('loadedmetadata', handleLoadedMetaData);
 video.addEventListener('timeupdate', handleTimeUpdate);
-video.addEventListener('mousemove', handleMouseMove);
-video.addEventListener('mouseleave', handleMouseLeave);
+video.addEventListener('click', handlePlay);
+videoContainer.addEventListener('mousemove', handleMouseMove);
+videoContainer.addEventListener('mouseleave', handleMouseLeave);
 timeline.addEventListener('input', handleInputTimeline);
 timeline.addEventListener('mousedown', handleTimelineMousedown);
 timeline.addEventListener('mouseup', handleTimelineMouseup);
