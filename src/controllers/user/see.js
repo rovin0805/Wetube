1f52f5fd495cd66c89e707de5835f9e34391716c
Choi Eun Jeong <qhdrn67@naver.com>
2023-01-12 17:46:56 +0900

    fix : profile video thumbnail

--- src/controllers/user/see.js
+++ src/controllers/user/see.js
@@ -3,7 +3,13 @@ import User from '../../models/User';
 const see = async (req, res) => {
   try {
     const { id } = req.params;
-    const user = await User.findById(id).populate('videos');
+    const user = await User.findById(id).populate({
+      path: 'videos',
+      populate: {
+        path: 'owner',
+        model: 'User',
+      },
+    });
     if (!user) {
       return res.status(400).render('404', { pageTitle: 'User Not Found 404' });
     }
